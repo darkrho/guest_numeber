bd8b376ddfe25b034bf768e0b5f710f3ee695825
darkrho <darkrho@outlook.es>
2024-03-25 16:01:06 +0100

    works but changes have to be done

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,8 @@ const  number_to_guest = [0,0,0,0,0].map( num => randomNum(num,10))
 
 function App() {
   
-  // input state
+  // input state and representation state
+
   const [guest, setNumber] = useState([0,0,0,0,0]);
   const [repre, setRepre] = useState(["*","*","*","*","*"])
 
@@ -25,19 +26,36 @@ function App() {
     setNumber(newGuest)
   }
   const handleValidation = (guest,number_to_guest) => {
-    console.log(guest, number_to_guest)
+    const newRepre = [...repre]
+    const up_repre = Array.from(document.getElementsByClassName("upRepre")[0].children)
+    const down_repre = Array.from(document.getElementsByClassName("downRepre")[0].children)
     guest.map( (num, idx) => {
       num = Number(num)
       if ( num === number_to_guest[idx]){
-        const newRepre = [...repre]
-        console.log(newRepre)
         newRepre[idx] = num
-        setRepre(newRepre)
-      }}
-    )
+        
+      }})
+
+    number_to_guest.map( (num, idx) => {
+      up_repre[idx].classList.remove("colorUpRed")
+      up_repre[idx].classList.remove("colorUpWhite")
+      down_repre[idx].classList.remove("colorDownWhite")
+      down_repre[idx].classList.remove("colorDownRed")
+    if ( guest[idx] > num){
+      up_repre[idx].classList.add("colorUpRed")
+      down_repre[idx].classList.add("colorDownWhite")
+
+    }else if (guest[idx] < num){
+      down_repre[idx].classList.add("colorDownRed")
+      up_repre[idx].classList.add("colorUpWhite")
+      
+        
+    }})
+    setRepre(newRepre)
   }
   
   // create numbers
+  
   
  
   return (
@@ -48,20 +66,40 @@ function App() {
         {number_to_guest.map( (num, idx) => {
           return <NumContainers key={idx} num={num} repre={repre[idx]} />
         })}
+      </div>
+      {/* is up hint */}
+      <div className="upRepre">
+        { number_to_guest.map( (_,idx) => {
+            return (<div key={idx} className={`up colorUpWhite`}>
+            </div>)
+
+          })
+        }
 
       </div>
-      
+
+
       {/* guest inputs */}
       <div className="number_user">
-        {number_to_guest.map( (num, idx) => {
+        {number_to_guest.map( (_, idx) => {
           return <InputComponent
           key={idx}
           num={idx} 
           handle={handleChange} />
         })}
       </div>
+      
+      {/* is down hint */}
+      <div className="downRepre">
+      { number_to_guest.map( (_,idx) => {
+            return (<div key={`down${idx}`} className={"down colorDownWhite"}>
+            </div>)
+
+          })
+        }
+      </div>
       <button onClick={ () => handleValidation(guest, number_to_guest)}>Validate</button>
-     
+
     </div>
   )
 }
